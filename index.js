33fda1087bbc0d3c0657f1251d9099e2e6168214
natan <natanr123@gmail.com>
2018-10-18 04:11:48 +0300

    fixed uglify problem

--- index.js
+++ index.js
@@ -1,55 +1,30 @@
-function addToScope(func, context) {
-
-    context[func.name] = (...args) => {
-        context.___currentData = func(context.___currentData, ...args);
+function localAddToScope(func, funcName, context) {
+    context[funcName] = (...args) => {
+        context.currentData = func(context.currentData, ...args);
         return context;
     };
-
-    context.finish = () => {
-        return context.___currentData;
-    };
+    context.finish = () => context.currentData;
     return context;
 }
 
-function initScope(initialArr, context) {
-    context.___currentData =  initialArr;
+function localInitScope(initialArr, context) {
+    context.currentData = initialArr;
     return context;
 }
 
-
-// @TODO clone the class instead of change it
 function withScope(theClass) {
-    theClass.scope = (arr) => { return initScope(arr, {})};
-
+    theClass.scope = (arr) => localInitScope(arr, {});
     theClass.addScope = function (funcName) {
         const oldScopeFunc = theClass.scope;
         theClass.scope = (arr) => {
             const funcToAdd = theClass[funcName];
             const context = oldScopeFunc(arr);
-            addToScope(funcToAdd, context);
+            localAddToScope(funcToAdd, funcName, context);
             return context;
         };
         return theClass;
     };
     return theClass;
 }
+
 module.exports = { withScope };
-/*
-// Old Code
-Function.prototype.scopable = function (funcName) {
-
-    if(this.scope === undefined) {
-        this.scope = (arr) => { return initScope(arr, {})};
-    }
-
-    const oldScopeFunc = this.scope;
-    this.scope = (arr) => {
-        const funcToAdd = this[funcName];
-        const context = oldScopeFunc(arr);
-        addToScope(funcToAdd, context);
-        return context;
-    };
-    return this;
-};
-*/
-
